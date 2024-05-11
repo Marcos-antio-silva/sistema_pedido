fce8a0b5fd3b77dfaab1de2de5be2c3d2828d450
Marcos-antio-silva <157077914+Marcos-antio-silva@users.noreply.github.com>
2024-05-11 00:52:25 -0300

    00:50
    serialização de senha e cripto e autenticador

---
+++ src/resources/clienteR.js
@@ -0,0 +1,27 @@
+require('../modls/cliente');
+const mongoose = require('mongoose');
+const modelo = mongoose.model('clientes');
+
+class Cliente {
+    static async criar(dados) {
+        return await new modelo(dados).save();
+    }
+    
+    static async buscar_todos() {
+        return await modelo.find({});
+    }
+    
+    static async buscar_porId(id) {
+        return await modelo.findOne({ _id: id });
+    }
+
+    static async atualizar(id, dados) {
+        return await modelo.findOneAndUpdate({ _id: id }, { set: dados});
+    }
+
+    static async deletar(id) {
+        return await modelo.findByIdAndRemove(id);
+    }
+}
+
+module.exports = Cliente;
---
+++ src/resources/funcionarioR.js
@@ -0,0 +1,36 @@
+require('../modls/cliente');
+const mongoose = require('mongoose');
+const modelo = mongoose.model('funcionarios');
+const cript = require('bcrypt')
+const salt = 30
+
+class Funcionario{
+    static async criar(dados){
+        let {senha} = dados;
+        let rest = await cript.rest(senha, salt)
+        senha = rest;
+        dados.senha = senha;
+        return await new modelo(dados).save();
+    }
+
+    static async autenticar (dados){
+        const{matricula} = dados;
+        const funcionario = await modelo.findOne({matricula});
+        return funcionario;
+    }
+
+    static async buscar_todos(){
+        return await modelo.find({});
+    }
+    static async buscar_porId(id){
+        return await modelo.findOne({ _id: id });
+    }
+    static async atualizar(id,dados){
+        return await modelo.findOneAndUpdateid(id, {set: dados});
+    } 
+    static async deletar(id){
+        return await modelo.findOneAndRemove(id);
+    }
+}
+
+module.exports = Funcionario;
